d9e8b924da9069aa20adc566ff40501f48f70b41
Pedro H <pedro_gonsalves@hotmail.com>
2023-05-02 01:17:37 -0300

    add: data to insert into database

--- src/main/scala/postgre/data.scala
+++ src/main/scala/postgre/data.scala
@@ -7,12 +7,16 @@ import java.time.LocalDate
 
 object Data {
 
+  /*BOOKS*/
+
   val nameOfTheWild = Book(
     id = 2L,
     book_name = "The name of the wild",
     id_author = 1L,
     release_date = LocalDate.of(2012, 4, 15),
-    length_in_pages = 625
+    length_page = 625,
+    style = "Physical"
+
   )
 
   val wiseManFear = Book(
@@ -20,8 +24,32 @@ object Data {
     book_name = "The wise man's fear",
     id_author = 1L,
     release_date = LocalDate.of(2008, 4, 15),
-    length_in_pages = 936
+    length_page = 936,
+    style = "E-book"
   )
 
+  val animalFarm = Book(
+    id = 3L,
+    book_name = "Animal Farm",
+    id_author = 2L,
+    release_date = LocalDate.of(1945,8,17),
+    length_page = 112,
+    style = "Physical"
+  )
+
+
+  /*AUTHORS */
+  val patrickRothfuss = Author(
+    id = 1L,
+    author_name = "Patrick Rothfuss"
+  )
+
+  val georgeOrwell = Author(
+    id = 2L,
+    author_name = "George Orwell"
+  )
+
+  val listAuthors = List(patrickRothfuss,georgeOrwell)
+  val listBooks = List(nameOfTheWild,wiseManFear,animalFarm)
 
 }
